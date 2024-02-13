06e4321a9323e7326521e462140d55b02ce9df8b
Ivo Nunes <inunes1904@gmail.com>
2024-02-13 21:57:20 +0000

    coach http response

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -6,24 +6,25 @@ export default {
   namespaced: true,
   state() {
     return {
+      lastFetch: null,
       coaches: [
         {
           id: 'c1',
-          firstName: 'Ivo',
-          lastName: 'Nunes',
+          firstName: 'Maximilian',
+          lastName: 'Schwarzmüller',
           areas: ['frontend', 'backend', 'career'],
           description:
-            "I'm Ivo Nunes!",
-          hourlyRate: 34
+            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30
         },
         {
           id: 'c2',
-          firstName: 'Ruben',
-          lastName: 'Nunes',
-          areas: [ 'career'],
+          firstName: 'Julie',
+          lastName: 'Jones',
+          areas: ['frontend', 'career'],
           description:
-            'I am Ruben Nunes.',
-          hourlyRate: 35
+            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30
         }
       ]
     };
